c488200e3885425da5df679f1daf479115de6bed
RavinduPonnamperuma <ravindu.ponnamperuma@gmail.com>
2023-05-09 10:28:04 +0530

    Add register form

---
+++ src/app/forme1/forme1.component.ts
@@ -0,0 +1,11 @@
+import { Component,  } from '@angular/core';
+
+
+@Component({
+  selector: 'app-forme1',
+  templateUrl: './forme1.component.html',
+  styleUrls: ['./forme1.component.css']
+})
+export class Forme1Component {
+
+}
